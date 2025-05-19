d12ff14016ab67c14031e1e8e13e5c3c0801140d
MaosnKwok <mhtkwok20@gmail.com>
2025-05-19 18:22:09 -0400

    added styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
     <>
-      <h1>Final Exam 343</h1>
-      <h2>Kwok, Mason</h2>
-      <h3>Spring 2025</h3>
-    
+      <h1 className="text- 3x1 font-bold text-gray-800 mb-2">Final Exam 343</h1>
+      <h2 className="text- 2x1 font-semibold text-gray-700 mb-1">Kwok, Mason</h2>
+      <h3 classNmae="text- x1 text-gray-600">Spring 2025</h3>
     </>
   );
 }
